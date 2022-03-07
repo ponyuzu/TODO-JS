0b6e47ed3829926dacc015c0648ea6a1ed78cc73
ponyuzu <yuzukaw@yahoo.co.jp>
2022-03-07 22:17:14 +0900

    リストの追加(テキスト)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // タグ生成
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  li.appendChild(div);
+  div.appendChild(p);
+
+  //　リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
